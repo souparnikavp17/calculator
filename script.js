615ae3e2c4b67270f27eed0236b9cacad72bcbeb
souparnikavp17 <souparnikavp17@gmail.com>
2026-06-15 16:18:04 +0530

    Implement basic arithmetic operations and AC functionality

---
+++ script.js
@@ -0,0 +1,99 @@
+let a = null, b = null, result=null
+let op = null
+let digits = document.querySelectorAll(".key.digit")
+let opers = document.querySelectorAll(".key.operator")
+let output = document.querySelector(".output")
+
+function reset(){
+    a=null,b=null, result=null,op=null
+}
+
+function display(n) {
+    output.innerText = n
+}
+function operate(o) {
+    if (b !== null) {
+        switch (op) {
+            case "+":
+                result = a + b
+                break
+
+            case "-":
+                result = a - b
+                break
+
+            case "*":
+                result = a * b
+                break
+
+            case "/":
+                if(b==0){
+                    display("INVALID")
+                    reset()
+                    return
+                }
+                result = a/b
+                break
+            case "%":
+                if(a!==null){
+                    a=a/100
+                    display(a)
+                    console.log(a)
+                    return
+                }else{
+                    b=b/100
+                    display(b)
+                    return 
+                }
+            case "=":
+                break
+        }
+        a=result, b=null
+        display(result)
+    }
+
+    op=o
+
+}
+
+
+
+
+function input(n) {
+    if (op !== null) {
+        if (b === null) {
+            b = Number(n)
+
+        } else {
+            b = b * 10 + Number(n)
+        }
+        display(b)
+    } else {
+        if (a === null) {
+            a = Number(n)
+
+        } else {
+            a = a * 10 + Number(n)
+        }
+        display(a)
+    }   
+
+}
+
+digits.forEach(digit => {
+    digit.addEventListener("click",()=>{
+       input(digit.dataset.value)})
+    }) 
+
+opers.forEach(oper => {
+    oper.addEventListener("click", () => {
+        operate(oper.dataset.value);
+    })
+})
+
+let ac= document.querySelector(".key.clear-btn.AC")
+ac.addEventListener("click",()=>{
+    display(" ")
+    reset()
+    
+})
