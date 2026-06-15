9bde8875f6ef58152c8ff11bef5a006085f60135
souparnikavp17 <souparnikavp17@gmail.com>
2026-06-15 17:07:03 +0530

    Fixed bug in simultaneous calculations

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let a = null, b = null, result=null
+let a = null, b = null, result = null
 let op = null
 let digits = document.querySelectorAll(".key.digit")
 let opers = document.querySelectorAll(".key.operator")
 let output = document.querySelector(".output")
 
-function reset(){
-    a=null,b=null, result=null,op=null
+function reset() {
+    a = null, b = null, result = null, op = null
 }
 
 function display(n) {
-    output.innerText = n
+    output.innerText = Math.floor(n*100000)/100000
 }
 function operate(o) {
-    if(o==="%"){
-         if(a!==null){
-                    a=a/100
-                    display(a)
-                    console.log(a)
-                    return
-                }else{
-                    b=b/100
-                    display(b)
-                    return 
-                }
+    if (o === "%") {
+        if (a !== null) {
+            a = a / 100
+            display(a)
+            console.log(a)
+            return
+        } else {
+            b = b / 100
+            display(b)
+            return
+        }
     }
     if (b !== null) {
         switch (op) {
@@ -39,23 +39,23 @@ function operate(o) {
                 break
 
             case "/":
-                if(b==0){
+                if (b == 0) {
                     display("INVALID")
                     reset()
                     return
                 }
-                result = a/b
+                result = a / b
                 break
-         
-               
+
+
             case "=":
                 break
         }
-        a=result, b=null
+        a = result, b = null
         display(result)
     }
 
-    op=o
+    op = o
 
 }
 
@@ -63,7 +63,7 @@ function operate(o) {
 
 
 function input(n) {
-    if (op !== null) {
+    if (op !== null && op !== "=") {
         if (b === null) {
             b = Number(n)
 
@@ -72,41 +72,41 @@ function input(n) {
         }
         display(b)
     } else {
-        if (a === null) {
+        if (a === null || op === "=") {
             a = Number(n)
 
         } else {
             a = a * 10 + Number(n)
         }
         display(a)
-    }   
+    }
 
 }
 
-function backspace()
-{
-    if(b===null){
-        a=Math.floor(a/10)
-        if(a===0){
+function backspace() {
+    if (b === null) {
+        a = Math.floor(a / 10)
+        if (a === 0) {
             display(" ")
-        }else{
+        } else {
             display(a)
         }
-        
-    }else{
-        b=Math.floor(b/10)
-        if(b===0){
+
+    } else {
+        b = Math.floor(b / 10)
+        if (b === 0) {
             display("")
-        }else{
+        } else {
             display(b)
         }
     }
 }
 
 digits.forEach(digit => {
-    digit.addEventListener("click",()=>{
-       input(digit.dataset.value)})
-    }) 
+    digit.addEventListener("click", () => {
+        input(digit.dataset.value)
+    })
+})
 
 opers.forEach(oper => {
     oper.addEventListener("click", () => {
@@ -114,14 +114,14 @@ opers.forEach(oper => {
     })
 })
 
-let ac= document.querySelector(".key.clear-btn.AC")
-ac.addEventListener("click",()=>{
+let ac = document.querySelector(".key.clear-btn.AC")
+ac.addEventListener("click", () => {
     display(" ")
     reset()
-    
+
 })
 
-let c=document.querySelector(".key.clear-btn.C")
-c.addEventListener("click",()=>{
+let c = document.querySelector(".key.clear-btn.C")
+c.addEventListener("click", () => {
     backspace()
 })
